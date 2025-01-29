2b60bca2af7b9b6f62b19ec856d24dc8bf6741c0
LuigiRac <lui.rac06@gmail.com>
2025-01-29 16:48:38 +0100

    header setting

--- src/components/Headercontent.jsx
+++ src/components/Headercontent.jsx
@@ -1,5 +1,27 @@
+import { NavLink } from "react-router-dom"
+
 export default function HeaderContent() {
     return (
-        <h1>HEADER</h1>
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <NavLink className="navbar-brand" to="/">Navbar</NavLink>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/HomePage">Home Page</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/AboutUs">About Us</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/PostList">Post List</NavLink>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+
+        </header >
     )
 }
