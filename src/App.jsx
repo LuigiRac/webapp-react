b1a8df0db5807441b3a0b7b99455e2be62c92dc1
LuigiRac <lui.rac06@gmail.com>
2025-01-29 17:11:07 +0100

    create AboutUs.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 
 import DefaultLayout from "./pages/DefaultLayout";
-import MainContent from "./components/MainContent"
+
 import HomePage from './pages/HomePage'
 import SingleMovie from './pages/SingleMovie'
 
@@ -14,7 +14,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route Component={DefaultLayout}>
-            <Route path="/" Component={MainContent} />
             <Route path="/HomePage" Component={HomePage} />
             <Route path="/SingleMovie" Component={SingleMovie} />
           </Route>
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+export default function AboutUs() {
+    const title = "About Us";
+
+    return (
+        <h1>
+            <div className="title">{title}</div>
+        </h1 >
+    )
+};
+
+
+
