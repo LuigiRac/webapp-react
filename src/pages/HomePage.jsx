63597288881ee18991ace0d847aadcdd1ff28120
LuigiRac <lui.rac06@gmail.com>
2025-01-30 14:06:04 +0100

    chaiamta server e aggiunta card (map)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,53 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
 export default function HomePage() {
-    const title = "Home Page";
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/movies")
+            .then(response => {
+                setMovies(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
 
     return (
         <h1>
-            <div>{title}</div>
+            <div>
+                <div className="d-flex justify-content-center gap-3 m-4">
+                    {
+                        movies.map((movie) => (
+                            <div className="card" style={{ width: '18rem' }} key={movie.id}>
+                                <img className="card-img-top" src={movie.image} alt={movie.title}></img>
+                                <div className="card-body">
+                                    <h5 className="card-title">{movie.title}</h5>
+                                    <p className="card-text">{movie.abstract}</p>
+                                    <a href="#" className="btn btn-primary" >Leggi di più</a>
+                                    {/* <button onClick={() => deletePost(movie.id)} className="btn btn-primary">Delete</button> */}
+
+                                </div>
+                            </div>
+                        ))
+                    }
+
+                </div >
+            </div>
         </h1 >
     )
 };
